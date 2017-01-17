3454d5f9b1260c9be0523d50f2f6ff12734eee5e
Haoxiang Yang <haoxiangyang89@gmail.com>
2017-01-16 17:12:43 -0800

    Added Pilot Data and Tested on Extensive Model
    Added Pilot Data and Tested on Extensive Model

--- ExtensiveStochModel.jl
+++ ExtensiveStochModel.jl
@@ -1,26 +1,30 @@
-using JuMP,Cbc,PyPlot,DataFrames,RDatasets;
+using JuMP,Cbc,DataFrames,RDatasets;
 
 function solveStochPre(N,ArrAdd,DeptAdd,capAdd,connAdd)
     capType = readdlm(capAdd,',',header = false);
     connMat = readdlm(connAdd,',',header = false);
     connMat = round(Int64,connMat);
     M,totalT = size(capType);
-    totalCapDict = Dict(1=>107,2=>97,3=>92,4=>84,
-                    5=>65,6=>59,7=>56,8=>51,
-                    9=>25,10=>23,11=>21,12=>20,
-                    13=>90,14=>82,15=>77,16=>71,
-                    17=>85,18=>77,19=>73,20=>67,
-                    21=>15,22=>14,23=>13,24=>12,
-                    25=>55,26=>50,27=>47,28=>43,
-                    29=>21,30=>19,31=>18,32=>16,
-                    33=>71,34=>64,35=>61,36=>56,
-                    37=>13,38=>12,39=>11,40=>10);
-    ArrCapDict = Dict();
-    DeptCapDict = Dict();
-    for i in 1:40
-        ArrCapDict[i] = round(totalCapDict[i]*60/107);
-        DeptCapDict[i] = round(totalCapDict[i]*70/107);
-    end
+    # full size dictionary
+    #totalCapDict = Dict(1=>107,2=>97,3=>92,4=>84,
+    #                5=>65,6=>59,7=>56,8=>51,
+    #                9=>25,10=>23,11=>21,12=>20,
+    #                13=>90,14=>82,15=>77,16=>71,
+    #                17=>85,18=>77,19=>73,20=>67,
+    #                21=>15,22=>14,23=>13,24=>12,
+    #                25=>55,26=>50,27=>47,28=>43,
+    #                29=>21,30=>19,31=>18,32=>16,
+    #                33=>71,34=>64,35=>61,36=>56,
+    #                37=>13,38=>12,39=>11,40=>10);
+
+    # pilot dictionary
+    totalCapDict = Dict(1=>6,2=>5,3=>3);
+    ArrCapDict = Dict(1=>4,2=>3,3=>2);
+    DeptCapDict = Dict(1=>4,2=>3,3=>2);
+    #for i in 1:40
+    #    ArrCapDict[i] = round(totalCapDict[i]*60/107);
+    #    DeptCapDict[i] = round(totalCapDict[i]*70/107);
+    #end
     capA = zeros(M,totalT);
     capD = zeros(M,totalT);
     capT = zeros(M,totalT);
